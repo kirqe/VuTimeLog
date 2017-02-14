f9a63f19d5ee7ef6810809f79f34bb460d7de714
railsr <ikll7@yahoo.com>
2017-02-14 20:57:13 +0400

    about and readme

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # timelog
 
-> This is time tracker ish sample app built with vue.js 2 and vuex to learn vue
+VuTimeLog is a timetracker ish sample app built with the following tools to learn some vue.js
 
-## Build Setup
+- [vue.js 2](https://github.com/vuejs/vue)
+- [vuex](https://github.com/vuejs/vuex)
+- [vue-router](https://github.com/vuejs/vue-router)
+- [rails 5](https://github.com/rails/rails)
+
+If you have more experience and found an error or see something that can be improved or just built wrong, feel free to create an issue or a pull request :)
+
+## Setup
 
 ``` bash
 npm install
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,19 @@
 <template lang="html">
-  <div>
-    <p>
-      TimeLog is a timetracker sample app built with vue.js 2, vuex and vue-router to learn vue.js.
+  <div class="about">
+    <h2>About</h2>
+    <p>VuTimeLog is a timetracker ish sample app built with the following tools to learn some vue.js<br/>
+
+    <ul class="tls">
+      <li><a href="https://github.com/vuejs/vue" target="_blank">vue.js 2</a></li>
+      <li><a href="https://github.com/vuejs/vuex" target="_blank">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-router" target="_blank">vue-router</a></li>
+      <li><a href="https://github.com/rails/rails" target="_blank">rails 5</a></li>
+    </ul>
+
+    If you have more experience and found an error or see something that can be improved or just built wrong, feel free to create an issue or a pull request :)
+
+    <h3>Here's a link to the repo: <a href="https://github.com/kirqe/VuTimeLog" target="_blank">https://github.com/kirqe/VuTimeLog</a></h3>
+
     </p>
   </div>
 </template>
@@ -11,5 +23,11 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+  .about {
+    font-size: 20px;
+  }
+  .tls {
+    margin: 20px 0;
+  }
 </style>
