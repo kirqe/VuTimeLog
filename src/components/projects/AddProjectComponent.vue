9f245b63c3f3b1392409f4fe7d8a164122ef076d
railsr <ikll7@yahoo.com>
2017-02-18 04:23:52 +0400

    add new proj form ref

--- src/components/projects/AddProjectComponent.vue
+++ src/components/projects/AddProjectComponent.vue
@@ -1,18 +1,25 @@
 <template lang="html">
-  <div v-show="isAdding" class="add-project ">
-    <form class="form-inline well">
-      <div class="row">
-        <div class="form-group col-md-6">
-          <input type="text" v-model="project.title" class="form-control" id="projectName" placeholder="Project name">
+  <div class="clearfix">
+    <div class="pull-right" v-show="!isAdding">
+    <button @click.prevent="toggleAdd"  type="button" name="button" class="btn btn-success">Add new project</button>
+    </div>
+
+    <div v-show="isAdding" class="add-project ">
+      <form class="form-inline well">
+        <div class="row">
+          <div class="form-group col-md-6">
+            <input type="text" v-model="project.title" class="form-control" id="projectName" placeholder="Project name">
+          </div>
+          <div class="form-group col-md-4">
+            <input type="number" v-model="project.rate" class="form-control" id="projectRate" placeholder="$/hour">
+          </div>
+          <div class="form-group col-md-2">
+            <button @click.prevent="saveProject(project)" type="submit" class="btn btn-success">Add</button>
+            <button @click.prevent="toggleAdd" type="submit" class="btn btn-link">Cancel</button>
+          </div>
         </div>
-        <div class="form-group col-md-4">
-          <input type="number" v-model="project.rate" class="form-control" id="projectRate" placeholder="$/hour">
-        </div>
-        <div class="form-group col-md-2">
-          <button @click.prevent="saveProject(project)" type="submit" class="btn btn-success form-control">Add</button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -22,14 +29,19 @@ import { mapActions } from 'vuex'
 export default {
   data () {
     return {
-      isAdding: true,
+      isAdding: false,
       project: {
         title: '',
         rate: ''
       }
     }
   },
-  methods: mapActions(['saveProject'])
+  methods: {
+    ...mapActions(['saveProject']),
+    toggleAdd () {
+      this.isAdding = !this.isAdding
+    }
+  }
 }
 </script>
 
