4ba29fc043c0ab7085514de32e546ec6d7c885a2
railsr <ikll7@yahoo.com>
2017-02-22 18:27:58 +0400

    ..

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,7 +1,6 @@
 <template lang="html">
   <div class="row">
     <div class="col-md-12">
-      <loader-component :list="projects"></loader-component>
       <projects-component :projects="projects"></projects-component>
     </div>
   </div>
@@ -9,13 +8,11 @@
 
 <script>
 import ProjectsComponent from '../components/projects/ProjectsComponent'
-import LoaderComponent from '../components/LoaderComponent'
 import { mapGetters, mapActions } from 'vuex'
 
 export default {
   components: {
-    ProjectsComponent,
-    LoaderComponent
+    ProjectsComponent
   },
   computed: mapGetters({
     projects: 'getProjects'
